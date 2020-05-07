21f28df5503422663165f1a8c0a51831183be9f8
Matthew Budd <matthew.le.budd@gmail.com>
2020-05-08 00:43:06 +1000

    Julian day number sorta working

--- app/src/main/java/dev/budd/seeastro/MainActivity.java
+++ app/src/main/java/dev/budd/seeastro/MainActivity.java
@@ -10,23 +10,50 @@ import com.opencsv.CSVReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    private boolean once = true;
+    //Belrose lat/long
+    int latHr = 33;
+    int latMin = 44;
+    int latSec = 10;
+
+    int longHr = 151;
+    int longMin = 12;
+    int longSec = 52;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ArrayList<SpaceObject> objects = readCSVIntoObjects();
+//        SpaceObject tester = objects.get(5849);
+//        SpaceObject tester2 = objects.get(6045);
+//        prettyPrintObject(tester);
+//        prettyPrintObject(tester2);
 
-        SpaceObject tester = objects.get(5849);
-        SpaceObject tester2 = objects.get(6045);
-       prettyPrintObject(tester);
-       prettyPrintObject(tester2);
+        Date currentTime = Calendar.getInstance().getTime();
+        Calendar now = Calendar.getInstance();
+        now.setTime(currentTime);
+        int year = now.get(Calendar.YEAR);
+        int month = now.get(Calendar.MONTH)+1;
+        int day = now.get(Calendar.DAY_OF_MONTH);
+        int hour = now.get(Calendar.HOUR_OF_DAY);
+        int minute = now.get(Calendar.MINUTE);
+        int second = now.get(Calendar.SECOND);
 
-
+        double ut = hour + minute / 60.0 + second / 3600.0;
+        //double JD = getJulDay(day, month, year, hour, minute, second);
+        double JD = getJulDay(day, month, year, ut);
+        System.out.println("JD: " + JD);
+        double longitude = longHr + (longMin / 60.0) + (longSec / 3600.0);
+        System.out.println("Longitude: " + longitude);
+        double GMST = gmSiderealTime(JD);
+        System.out.println("GMST: " + GMST);
+        double LMST = 24.0 * remainder((GMST + longitude / 15) / 24);
+        System.out.println("LMST: " + LMST);
 
     }
 
@@ -53,13 +80,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /**
-     * I want to change this to take a space pbject object
-     * and pretty print relevant info.
-     * @param r
-     * @param d
-     */
-
     private void prettyPrintObject(SpaceObject o){
         System.out.print("Catalogue Name: " + o.getName());
         if(!o.getCommonName().matches("")){
@@ -72,18 +92,18 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("-----------------------------------------");
     }
     private void printRADec(SpaceObject o){
-        Double ra = Double.parseDouble(o.getRA());
-        Double dec = Double.parseDouble(o.getDec());
+        double ra = Double.parseDouble(o.getRA());
+        double dec = Double.parseDouble(o.getDec());
 
-        Double raDeg = Math.floor(Math.toDegrees(ra) / 15);
+        double raDeg = Math.floor(Math.toDegrees(ra) / 15);
         Double temp = ((Math.toDegrees(ra) / 15) - raDeg) * 60;
         Double raMin = Math.floor(temp);
-        Long raSec = Math.round((temp - raMin) * 60);
+        long raSec = Math.round((temp - raMin) * 60);
 
         Double decDeg = Math.floor(Math.toDegrees(Math.abs(dec)));
         Double temp2 = ((Math.toDegrees(Math.abs(dec))) - decDeg) * 60;
         Double decMin = Math.floor(temp2);
-        Long decSec = Math.round((temp2 - decMin) * 60);
+        long decSec = Math.round((temp2 - decMin) * 60);
         decDeg = (dec < 0) ? decDeg *= -1 : decDeg;
         System.out.println("-Coordinates: RA: " + raDeg + ":"+ raMin + ":"+ raSec + " Dec: "  + decDeg + ":"+ decMin + ":"+ decSec);
     }
@@ -96,6 +116,41 @@ public class MainActivity extends AppCompatActivity {
         }else{
             System.out.println("No VMag or BMag data.");
         }
+    }
+
+    /**
+     * BLEGH
+     */
+    //private double getJulDay(int d, int m, int y, int hr, int min, int sec){
+    private double getJulDay(int d, int m, int y, double u){
+        //System.out.println("CalculatingJD: " + " y: " + y + " m: " + m + " d: " + d + " hr: " + hr + " min: " + min + " sec: " + sec);
+        System.out.println("CalculatingJD: " + " y: " + y + " m: " + m + " d: " + d + " ut: " + u);
+        if (m <= 2){
+            m+=12;
+            y-=1;
+        }
+
+        return Math.floor(365.25 * (y + 4716)) + Math.floor(30.6001 * (m + 1)) + d - 13 - 1524.5 + u / 24.0;
+//        double JDN = (1461 * (y + 4800 + (m - 14)/12))/4 + (367 * (m - 2 - 12 * ((m - 14)/12)))/12 - (3 * ((y + 4900 + (m - 14)/12)/100))/4 + d - 32075;
+//        JDN = JDN + ((hr-12) / 24) + (min / 1440) + (sec / 86400.0);
+//        return JDN;
+    }
+
+    private double gmSiderealTime(double jd){
+        double t_eph, ut, MJD0, MJD;
+        MJD = jd - 2400000.5;
+        MJD0 = Math.floor(MJD);
+        ut = (MJD - MJD0) * 24.0;
+        t_eph = (MJD0 - 51544.5) / 36525.0;
+        return 6.697374558 + 1.0027379093 * ut + (8640184.812866 + (0.093104 - 0.0000062 * t_eph) * t_eph) * t_eph / 3600.0;
+    }
+
+    private double remainder(double x){
+        x=x-Math.floor(x);
+        if(x < 0){
+            x++;
+        }
+        return x;
     }
 
 }
